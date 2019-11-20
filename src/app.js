8467bee8588cf54a4987da2950e3ad5e1c52b43e
Jeremy Newcomb <jeremy.newcomb@tapslots.com>
2019-11-19 16:08:36 -0800

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const geoCode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Define paths for express config
 const publicDir = path.join(__dirname, "../public");
@@ -88,6 +88,7 @@ app.get("*", (req, res) => {
         error: "Page Not Found"
     });
 });
+
 
 app.listen(port, () => {
     console.log("Server is running on port " + port);
