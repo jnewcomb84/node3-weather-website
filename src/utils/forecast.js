63b42e9c86ff506f707fb8f4fe6ef5f289d01be2
Jeremy Newcomb <jeremy.newcomb@tapslots.com>
2019-11-19 16:54:18 -0800

    Added new 'feels like' data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,14 @@
 const request = require('request');
 
+//
+// Goal: Add new data to forecast
+//
+// 1. Update the forecast string to include new data
+// 2. Commit your changes
+// 3. Push your changes to GitHub and deploy to Heroku
+// 4. Test your work in the live application!
+//
+
 const forecast = (longitude, latitude, cb) => {
     const url = "https://api.darksky.net/forecast/0d0c7132b0a82561f85815526c478022/" + encodeURIComponent(latitude) + "," + encodeURIComponent(longitude) + "?units=us&exclude=minutely,hourly,alerts,flags";
 
@@ -11,7 +20,7 @@ const forecast = (longitude, latitude, cb) => {
              cb("Unable to find the given location");
          }
          else {
-            cb(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`);
+            cb(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out, but feels like ${body.currently.apparentTemperature}. There is a ${body.currently.precipProbability}% chance of rain.`);
          }
     });
 };
